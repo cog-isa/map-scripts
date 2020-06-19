ea5740a0cd21a8ef1b011ab67d7cbd6fddc5e3df
ASEDOS999 <kuruzov2014@mail.ru>
2020-06-19 11:17:02 +0300

    Update test: output signs names

--- Demonstration/test.py
+++ Demonstration/test.py
@@ -12,3 +12,6 @@ name_table = 'simple_auto_table.pickle'
 path = "../Texts/"
 list_files = [path + 'text0_0.txt']
 S, actions_sign, role_sign, obj_sign, signifs = create_script_sign(list_files, name_table)
+print("\nActions names:", actions_sign.keys())
+print("\nRoles:", role_sign.keys())
+print("\nPlaceholders:", obj_sign.keys())
