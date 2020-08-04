d730adb623d11d39304cc2342e4845108a0d6315
ASEDOS999 <kuruzov2014@mail.ru>
2020-08-04 03:10:48 +0300

    Change visual

--- Demonstration/test.py
+++ Demonstration/test.py
@@ -7,15 +7,21 @@ Created on Mon Jun 15 15:32:18 2020
 """
 from ScriptExtract.GraphScript.graph_construction import create_script_sign
 
+def print_list(name, l, start = "\t"):
+    print(name)
+    for ind, i in enumerate(l):
+        print(start + str(ind) + ". " + i)
+
 name_table = 'simple_auto_table.pickle'
 path = "../Texts/"
 list_files = [path + 'text0_0.txt']
 S, actions_sign, role_sign, obj_sign, char_sign, signifs = create_script_sign(list_files, name_table)
-print("\nActions names:", actions_sign.keys())
-print("\nRoles:", role_sign.keys())
-print("\nCharacteristics:", char_sign.keys())
-print("\nPlaceholders:", obj_sign.keys())
+print_list("\nActions names:", actions_sign.keys())
+print_list("\nPredicates:", role_sign.keys())
+print_list("\nCharacteristics:", char_sign.keys())
+print_list("\nPlaceholders:", obj_sign.keys())
 print()
+
 for key in S.significances:
     signif = S.significances[key]
     print(signif.index, signif.cause)
